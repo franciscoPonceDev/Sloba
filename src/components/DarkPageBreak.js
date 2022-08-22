b38921076ba46f28eed0ae55520872e0d52928a6
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-08-22 12:08:40 -0300

    deleting useless files

--- src/components/DarkPageBreak.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DarkPageBreak = () => (
-  <div>
-    <div className="bg-gray-800 dark-triangle w-full" />
-  </div>
-);
-
-export default DarkPageBreak;
--- src/components/ReverseDarkPageBreak.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ReversedDarkPageBreak = () => (
-  <div>
-    <div className="bg-gray-800 reverse dark-triangle w-full" />
-  </div>
-);
-
-export default ReversedDarkPageBreak;
